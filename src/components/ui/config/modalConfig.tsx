d78e05372ca2005e634d70c9c9a7b734af3d5d01
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-11-04 10:28:38 +0530

    Implement risk acceptance in authSlice and refine API endpoints
    This update adds a `riskAccepted` state to the `authSlice`. This new attribute will be used to track whether a user has admitted a risk disclosure or not. The refactor includes improvements on various API endpoints and modifies the file structure for better organization. A new module, `apiEndpoint.ts`, is added to generate dynamic API endpoint URLs. Enhancements have also been made in cookie management in the `verify-otp` route.

---
+++ src/components/ui/config/modalConfig.tsx
@@ -0,0 +1,63 @@
+import type * as CSS from "csstype";
+import type { ModalFuncProps } from 'antd/lib/modal'
+
+
+export const DefaultCenteredContentStyle = {
+  body: {
+    padding:"auto",
+  },
+  content:{
+    height:"100%"
+  },
+  
+};
+export const DefaultCenteredModalStyle: CSS.Properties = {
+  padding:0
+}
+export const DefaultCenteredCustomProps: ModalFuncProps = {
+  footer:null,
+  closeIcon:null,
+  centered:true
+  // title:<div className={"text-xl"}>
+  //   Hey
+  //   </div>
+  
+}
+
+
+export const InvestContentStyle= {
+  body: {
+    padding:0,
+    margin:0,
+  },
+  content:{
+    height:"100%"
+  },
+  header:{
+    fontSize:"24px"
+  }
+}
+export const InvestModalStyle:CSS.Properties= {
+  position:"fixed",
+  right: 0,
+  top: 0,
+  bottom:0,
+  margin:0 ,
+  padding:0
+}
+export const InvestModalCustomProps:ModalFuncProps={
+  footer:null,
+  closeIcon:null,
+  // title:<div className={"text-xl"}>
+  //   Hey
+  //   </div>
+  
+}
+
+
+export const RiskDisclosureContentStyle={
+}
+export const RiskDisclosureModalStyle:CSS.Properties={}
+export const RiskDisclosureCustomProps:ModalFuncProps={
+
+}
