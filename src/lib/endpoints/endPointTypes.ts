9835a43b005bcc6308e9a88b0dac664186f40c05
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-11-04 10:26:50 +0530

    Add new endpoint types in endPointTypes.ts file
    Introduced new types 'EndpointProps' and 'ICheckInterest' in endPointTypes.ts to improve structure and readability. 'EndpointProps' will handle the necessary properties for endpoints while 'ICheckInterest' acts as an interface for checking interests in a startup, enabling cleaner and organized codes.

---
+++ src/lib/endpoints/endPointTypes.ts
@@ -0,0 +1,12 @@
+export interface EndpointProps {
+  baseUrlExist?:boolean,
+  params?:ICheckInterest,
+  data?:any,
+  token?:string
+}
+export interface ICheckInterest{
+  [key: string]: string;
+  startupId: string,
+  investorId: string,
+}
+
