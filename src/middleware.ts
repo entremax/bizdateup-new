63dfbd342c7f26eb363ec5c1c5529732ef91b082
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-12-05 00:38:33 +0530

    Refactor middleware for user roles and routes
    The middleware has been refactored to manage routes based on user roles (investor, startup, admin). The authentication process has been updated to consider not only the presence of a token, but also the assigned role and specific access routes. A separate collection of publicPaths has been defined for general access regardless of authentication.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,56 @@
 // middleware.ts
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
+import { UserRole } from '@/types'
 
-const pathType = {
-  unAuthenticated: [/\/login/, /\/signup/, /\/otp.*/],
-  authenticated: [
-    /\/dashboard.*/,
+const authenticated: { [key in UserRole]: RegExp[] } = {
+  investor: [
+    /\/dashboard\/investor.*/,
     /\/invest.*/,
-    /\/profile.*/,
+    /\/profile\/investor.*/,
     /\/transactions.*/,
     /\/referral.*/,
   ],
+  startup: [/\/dashboard\/startup.*/, /\/profile\/startup.*/],
+  admin: [/\/dashboard\/investor.*/],
 }
+const publicPaths = [/^\/$/, /\/login/, /\/signup/, /\/otp.*/]
 
 export function middleware(req: NextRequest) {
-  let token = req.cookies.get('token')
+  const token = req.cookies.get('token')
+  const role = req.cookies.get('role')?.value as UserRole
   const path = req.nextUrl.pathname
+  const url = req.nextUrl.clone()
+  const matchPath = (patterns: RegExp[]) =>
+    patterns.some((pattern) => pattern.test(path))
 
-  const matchPath = (pathList: RegExp[]) => {
-    return pathList.some((pattern) => pattern.test(path))
+  if (
+    (token &&
+      role &&
+      role in authenticated &&
+      matchPath(authenticated[role])) ||
+    matchPath(publicPaths)
+  ) {
+    return NextResponse.next()
   }
-  if (token && matchPath(pathType.unAuthenticated)) {
-    const url = req.nextUrl.clone()
-    url.pathname = '/dashboard'
+
+  if (token) {
+    url.pathname = `/dashboard/${role}`
     return NextResponse.redirect(url)
   }
-  if (!token && matchPath(pathType.authenticated)) {
-    const url = req.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.rewrite(url)
-  }
-  return NextResponse.next()
+  url.pathname = `/login`
+  return NextResponse.rewrite(url) // this line is reached if the token is not found
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|logout|me|verify-otp|_next/static|_next/image|favicon.ico).*)',
+  ],
 }
