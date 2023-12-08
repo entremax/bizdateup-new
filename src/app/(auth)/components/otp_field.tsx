40c20051de7926bceb1e924ceb064a642a98f02b
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-12-08 18:53:14 +0530

    Remove unnecessary router refresh in OTP field
    In this commit, the redundant code line that refreshes the router in the OTP field component is removed. This improves the efficiency of the code and redirection logic, as after validation the user will now be directly redirected to the dashboard, eliminating unneeded extra steps.

--- src/app/(auth)/components/otp_field.tsx
+++ src/app/(auth)/components/otp_field.tsx
@@ -182,7 +182,6 @@ export default function OtpField({ id }: { id: string }) {
               premiumMember: investorData.membership.isMember !== 'no',
             },
           })
-          router.refresh()
           return router.push('/dashboard')
         }
       }
