7dda687a83ff68c8775fd86e64bb7d7f8411630c
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-12-14 12:09:56 +0530

    draft commit for google login
    added the required pages and api routes structure for social login created a socialLogin routes which will enable login using google

--- src/app/socialLogin/page.tsx
+++ src/app/socialLogin/page.tsx
@@ -1,3 +1,17 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
+
 export default function SocialLogin() {
-  return <div> g</div>
+  const searchParams = useSearchParams()
+  const role = searchParams.get('role')
+  const refId = searchParams.get('refid')
+  const type = searchParams.get('type')
+  const token = searchParams.get('token')
+
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+      <div className="loader"></div>
+    </div>
+  )
 }
