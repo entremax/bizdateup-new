90e19c0c1f621c9f0dd8d362acd2dfce83b831a1
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-12-16 18:16:38 +0530

    added loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+      <div className="loader"></div>
+    </div>
+  )
+}
--- src/components/navbar/startup_updates.tsx
+++ src/components/navbar/startup_updates.tsx
@@ -5,11 +5,12 @@ import StartupUpdate from '@/components/StartupUpdate'
 export default function StartupUpdatesDropDown() {
   const { updates } = useAppSelector((state) => state.startup)
   return (
-    <div className="border_gray min-h-[14rem] w-[24rem] rounded-lg bg-white px-2 shadow-lg">
+    <div className="border_gray min-h-[14rem] w-[28rem] rounded-lg bg-white px-2 shadow-lg">
       <div className={'flex items-center justify-between p-4 px-3'}>
         <h2 className={'text-xl font-semibold'}>Startup Updates</h2>
         <Button
-          type="text"
+          type="link"
+          href={'/startup/updates'}
           className={'!text-sm font-medium !text-primary'}
           size={'small'}>
           Show All
