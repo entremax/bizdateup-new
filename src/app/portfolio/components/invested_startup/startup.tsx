99b9ed26b4e87c483581352375b5f340b58077cd
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-12-24 17:08:31 +0530

    added invested startups details list in portfolio

--- src/app/portfolio/components/invested_startup/startup.tsx
+++ src/app/portfolio/components/invested_startup/startup.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { InvestedStartup } from '@/types/portfolio'
 import Image from 'next/image'
 import {
   apiUri,
   capitalizeFirstLetter,
+  formatCustomDate,
   formatIndianValuation,
   formatNumberWithDecimal,
 } from '@/lib/utils'
 import { StartupTag } from '@/components/tag'
+import { Button } from 'antd'
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons'
+import AccreditationData from '@/components/portfolio/invested_startup/AccreditationData'
 
 type Props = {
   details: InvestedStartup
 }
 
 const StartupDetails: React.FC<Props> = ({ details }) => {
+  
+  const [show, setShow] = useState(false)
   function numberOfShares() {
-    const investedAmount = details.investedAmount || 0
-    const sharePrice = parseFloat(details.portfolio.shareprice) || 0
+    const investedAmount = details?.investedAmount || 0
+    const sharePrice = parseFloat(details?.portfolio?.shareprice) || 0
     if (sharePrice !== 0)
       return formatNumberWithDecimal(investedAmount / sharePrice)
     else return 0
@@ -24,9 +31,9 @@ const StartupDetails: React.FC<Props> = ({ details }) => {
 
   function calculateCurrentInvestment() {
     const currentValuation =
-      parseFloat(details.portfolio?.currentValuation) || 0
-    const investedAmount = details.investedAmount || 0
-    const investedValuation = details.dealTerms?.valuation || 0
+      parseFloat(details?.portfolio?.currentValuation) || 0
+    const investedAmount = details?.investedAmount || 0
+    const investedValuation = details?.dealTerms?.valuation || 0
 
     return formatNumberWithDecimal(
       (currentValuation * investedAmount) / investedValuation,
@@ -43,9 +50,9 @@ const StartupDetails: React.FC<Props> = ({ details }) => {
   return (
     <div
       className={
-        'border_gray grid grid-cols-12 items-center gap-4 rounded-xl p-4'
+        'border_gray grid grid-cols-12 items-center gap-4 rounded-xl p-4 shadow'
       }>
-      <div className="col-span-full flex items-center gap-4 md:col-span-4">
+      <div className="col-span-full flex flex-wrap items-center gap-4 md:flex-nowrap">
         <div className="border_gray relative min-h-[3rem] min-w-[3rem] max-w-[3rem]">
           <Image
             src={`${apiUri().v1}/logo/${details.logo}`}
@@ -53,23 +60,19 @@ const StartupDetails: React.FC<Props> = ({ details }) => {
             layout="fill"
           />
         </div>
-        <div className="flex flex-col gap-2">
+        <div className="flex min-w-[14rem] flex-col gap-2 lg:min-w-[28rem]">
           <h3 className="flex-warp truncate text-ellipsis break-words text-xl lg:hidden">
-            {capitalizeFirstLetter(
-              details.registeredCompanyName.split(' '),
-            ).slice(0, 2)}
+            {capitalizeFirstLetter(details.registeredCompanyName.split(' '))}
             ...
           </h3>
           <h3 className="flex-warp hidden truncate text-ellipsis break-words text-xl lg:flex">
-            {capitalizeFirstLetter(
-              details.registeredCompanyName.split(' '),
-            ).slice(0, 3)}
+            {capitalizeFirstLetter(details.registeredCompanyName.split(' '))}
             ...
           </h3>
           <StartupTag tags={details.tags} />
         </div>
-        <div className="col-span-full grid grid-cols-2 items-center gap-8 md:col-span-8 md:grid-cols-5">
-          <div className="flex flex-col gap-2">
+        <div className="col-span-full grid w-full min-w-[10rem] grid-cols-2 items-center justify-around gap-8 md:col-span-8 md:flex">
+          <div className="flex flex-col items-start gap-2">
             <p className="text-sm text-neutral-400">Current Value</p>
             <span className="cursor-pointer text-base font-semibold">
               {details?.portfolio?.currentValuation
@@ -80,14 +83,31 @@ const StartupDetails: React.FC<Props> = ({ details }) => {
                 : '-'}{' '}
             </span>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col items-start gap-2">
             <p className="text-sm text-neutral-400">Invested Amount</p>
             <span className="cursor-pointer text-base font-semibold">
               {details.investedAmount}
             </span>
           </div>
+          <div className="flex flex-col items-start gap-2">
+            <p className="text-sm text-neutral-400">Shares</p>
+            <span className="cursor-pointer text-base font-semibold">
+              {numberOfShares()}
+            </span>
+          </div>
+          <div className="flex flex-col items-start gap-2">
+            <p className="text-sm text-neutral-400">Last Invested</p>
+            <span className="cursor-pointer text-base font-semibold">
+              {formatCustomDate(details.investedDate)}
+            </span>
+          </div>
+          <Button
+            icon={show ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+            onClick={() => setShow(!show)}
+          />
         </div>
       </div>
+      <AccreditationData details={details} show={show} />
     </div>
   )
 }
