de6a3a3691c793116e67565145d30d062fcfeed8
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2023-12-30 10:44:36 +0530

    don't know what I fixed

--- src/app/referral/components/table/index.tsx
+++ src/app/referral/components/table/index.tsx
@@ -2,49 +2,50 @@
 import Startup from '@/components/referral/table/Startup'
 import Investor from '@/components/referral/table/Investor'
 import SearchAndFilter from '@/components/referral/table/search'
-import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import ExportButton from '@/components/referral/table/ExportButton'
 import { useSearchParams } from 'next/navigation'
 import { useAppSelector } from '@/store/hooks'
+import { useState } from 'react'
+import { Button } from 'antd'
 
 type Props = {
   searchParams: { [key: string]: string | string[] | undefined }
 }
+const links = [
+  { label: 'Investors', tab: 'investor' },
+  { label: 'Startups', tab: 'startups' },
+]
 export default function ReferralTransactions() {
   const searchParams = useSearchParams()
-  const tableType = searchParams?.get('tab')
+  const [tabType, setTab] = useState(links[0].tab)
   const { accelerator } = useAppSelector(({ accelerator }) => accelerator)
-  const links = [
-    { label: 'Investors', tab: 'investor' },
-    { label: 'Startups', tab: 'startups' },
-  ]
+
   return (
     <div className="flex scroll-pt-20 flex-col gap-4 p-4">
       <div className="my-2 flex items-center justify-between">
         <div className="flex h-full gap-4">
           {links.map(({ label, tab }) => (
-            <Link
+            <Button
               key={tab}
-              scroll={false}
-              href={`/referral${tab ? '?tab=' + tab : ''}`}
+              onClick={() => setTab(tab)}
               className={cn(
                 `text-lg font-semibold text-neutral-500 ${
-                  (tableType === tab || (!tableType && tab === 'investor')) &&
+                  (tabType === tab || (!tabType && tabType === 'investor')) &&
                   'border-0 border-b-4 border-solid border-b-primary text-primary '
                 } `,
               )}>
               {label}
-            </Link>
+            </Button>
           ))}
         </div>
         <div className="font-medium text-primary">
-          <ExportButton table={tableType} />
+          <ExportButton table={tabType} />
         </div>
       </div>
 
       {accelerator && <SearchAndFilter acceleratorData={accelerator} />}
-      {!tableType || tableType !== 'startups' ? <Investor /> : <Startup />}
+      {!tabType || tabType !== 'startups' ? <Investor /> : <Startup />}
     </div>
   )
 }
