df0cd1a69570fe920dd153f1f7cbefed758341e7
Kalyan Jyoti Borah <kalyanborah456@gmail.com>
2024-01-11 12:09:40 +0530

    fixing  again

--- src/app/dashboard/startup/_component/companyTarget.tsx
+++ src/app/dashboard/startup/_component/companyTarget.tsx
@@ -19,7 +19,7 @@ export default function CompanyTarget({ startup }: { startup: StartupData }) {
     <>
       <div className="border_gray mx-3 flex flex-col gap-3 rounded-xl p-4 shadow-sm md:mx-14 md:flex-col lg:mx-32">
         <div className='fle" flex-row justify-between'>
-          "{' '}
+          {' '}
           <div className="text-black-400 flex flex-col justify-between text-lg font-bold">
             {/* <span>0</span>{' '} */}
             <span>
